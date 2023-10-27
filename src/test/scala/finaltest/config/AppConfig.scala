667500e3fdc5452731ac81a6630360db39d492c0
pdylewski <piotr.dylewski@external.t-mobile.pl>
2023-10-27 13:29:12 +0200

    Jenkins code-push-change test

--- src/test/scala/finaltest/config/AppConfig.scala
+++ src/test/scala/finaltest/config/AppConfig.scala
@@ -4,6 +4,6 @@ object AppConfig {
 
   val USER_COUNT = System.getProperty("USER_COUNT","4").toInt
   val RAMP_DURATION = System.getProperty("RAMP_DURATION","8").toInt
-  val TEST_DURATION = System.getProperty("TEST_DURATION","30").toInt
+  val TEST_DURATION = System.getProperty("TEST_DURATION","32").toInt
 
 }
